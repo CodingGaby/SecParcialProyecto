fa42977f146c2e22e0e79127c488e874f2fc74ee
Gaby Osorio <macma.cube1@gmail.com>
2023-09-30 12:18:46 -0600

    Menu agregado

--- src/main/java/com/mycompany/Main.java
+++ src/main/java/com/mycompany/Main.java
@@ -1,18 +1,31 @@
 package com.mycompany;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner inp = new Scanner(System.in);
         Sort sort = new Sort();
 
+        System.out.println("Bienvenidx!");
 
-        int[] arr = {2,3,4,1,5};
-        int[] arr2 = {123,834,94,4854,1242};
+        while (true){
+            System.out.println("\nElige que programa quieres usar ^^\n\t\ta.Ordenar arreglos \n\t\tb.Juegos \n\t\t\tq.Salir");
+            int opc = inp.next().charAt(0);
 
-
-        sort.bubble(arr2);
-        sort.insertion(arr);
-
-        sort.printArray(arr);
-        sort.printArray(arr2);
+            switch (opc){
+                case 'a' -> {
+                    System.out.println("La opcion es A");
+                }
+                case 'b' -> {
+                    System.out.println("La opcion es B");
+                }
+                case 'q' ->{
+                    System.out.println("Saliendo del programa...");
+                    System.exit(0);
+                }
+                default -> System.out.println("Opcion invalida, ingresa una opcion valida;c");
+            }
+        }
     }
 }
--- src/main/java/com/mycompany/Sort.java
+++ src/main/java/com/mycompany/Sort.java
@@ -50,14 +50,15 @@ public class Sort {
     }
 
     public void insertion(int[] arr){
-        for(int i = 0; i < arr.length; i++){
-            for (int j = i+1; j < arr.length; j++){
-                if (arr[i] > arr[j]){
-                    int temp = arr[i];
-                    arr[j] = arr[i];
-                    arr[i] = temp;
-                }
+        for(int i = 1; i < arr.length; i++){
+            int temp = arr[i];
+
+            int j = i - 1;
+            while (j >= 0 && arr[j] > temp){
+                arr[j+1] =arr[j];
+                j--;
             }
+            arr[j+1] = temp;
         }
     }
 }
